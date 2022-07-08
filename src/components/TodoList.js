641b7f35e595f8481e4fbd491a8ffd86c5142bca
MyTestsubject <64662693+MyTestsubject@users.noreply.github.com>
2022-07-08 00:01:50 -0400

    1:04:07

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,19 @@
 import React from "react";
-
-const TodoList = () => {
+import Todo from "./Todo";
+const TodoList = ({ setTodos, todos }) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
